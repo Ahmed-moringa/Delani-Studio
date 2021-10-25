60841a06872f03bf76c4d99f4a25ebe7244c494e
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2021-10-25 21:59:50 +0300

    change slidedown to toggel fast in what we do container

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,36 @@
 $(document).ready(function(){
     $("#development-image").click(function(){
-        $("#development-image").slideDown('1500').hide('1000');
-        $("#development").show('1500');
+        $("#development-image").slideToggle('fast');
+        $("#development").slideToggle('fast');
     });
     $("#development").click(function(){
-        $("#development").slideUp('1500');
-        $("#development-image").slideDown('1500');
+        $("#development").slideToggle('fast');
+        $("#development-image").slideToggle('fast');
     });
 });
-  
+
 $(document).ready(function(){
     $("#design-image").click(function(){
-        $("#design-image").slideDown('1500').hide('1000');
-        $("#design").show('1500');
+        $("#design-image").slideToggle('fast');
+        $("#design").slideToggle('fast');
     });
     $("#design").click(function(){
-        $("#design").slideUp('1500');
-        $("#design-image").slideDown('1500');
+        $("#design").slideToggle('fast');
+        $("#design-image").slideToggle('fast');
     });
 });
   
 $(document).ready(function(){
     $("#product-image").click(function(){
-        $("#product-image").slideDown('1500').hide('1000');
-        $("#product").show('1500');
+        $("#product-image").slideToggle('fast');
+        $("#product").slideToggle('fast');
     });
     $("#product").click(function(){
-        $("#product").slideUp('1500');
-        $("#product-image").slideDown('1500');
+        $("#product").slideToggle('fast');
+        $("#product-image").slideToggle('fast');
     });
 });
-  
-$(document).ready(function(){
-    $("#work1").mouseover(function(){
-        $("#overlay").show();
-    }).mouseout(function(){
-        $("#overlay").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work2").mouseover(function(){
-        $("#overlay2").show();
-    }).mouseout(function(){
-        $("#overlay2").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work3").mouseover(function(){
-        $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
-    });
-});
-  
+
 $(document).ready(function(){
     $("#work5").mouseover(function(){
       $("#overlay5").show();
@@ -82,20 +53,3 @@ $(document).ready(function(){
       $("#overlay8").hide();
     });
 });
-  
-$(document).ready(function(){
-    $("form#form34A").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#MERGE1").val();
-      var email = $("input#MERGE0").val();
-      var message = $("textarea#comment").val();
-      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
-      
-    });
-  
-  });
